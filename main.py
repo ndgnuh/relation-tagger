a5672c04fbc7b5aa4e95da38535da66d59d2a566
Nguyễn Đức Hùng <ndgnuh@protonmail.com>
2022-06-01 23:22:14 +0700

    more UI

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import spade_label_tool.state as State
-import pandas as pd
-from lenses import bind
+from spade_label_tool import state as State
+from spade_label_tool import static_ui
 import pygame_gui as pgui
 import pygame as pg
 from pygame.locals import *
@@ -12,14 +11,22 @@ def main():
     pg.display.set_caption("Spade label tool")
     root_sf = pg.display.set_mode((800, 600), RESIZABLE)
     manager = pgui.UIManager(root_sf.get_size())
+    ui = static_ui.draw(manager)
+
+    clock = pg.time.Clock()
     while state.is_running.get():
+        time_delta = clock.tick(60) / 1000.0
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 state = State.stop(state)
 
+            manager.process_events(event)
+
+        manager.update(time_delta)
         background = pg.Surface(root_sf.get_size())
-        background.fill(pg.Color(90, 90, 90))
+        background.fill(pg.Color(31, 31, 31))
         root_sf.blit(background, (0, 0))
+        manager.draw_ui(root_sf)
         pg.display.update()
 
 
---
+++ spade_label_tool/static_ui.py
@@ -0,0 +1,25 @@
+import pygame as pg
+import pygame_gui as pgui
+from pygame.locals import *
+from argparse import Namespace
+
+
+def draw(manager):
+    ns = Namespace()
+    x = 0
+    ns.import_label_btn = pgui.elements.UIButton(
+        relative_rect=pg.Rect((0, 0), (-1, 50)),
+        text='Import label',
+        manager=manager)
+    x += ns.import_label_btn.relative_rect.width
+    ns.import_btn = pgui.elements.UIButton(
+        relative_rect=pg.Rect((x, 0), (100, 50)),
+        text='Import',
+        manager=manager)
+    x += ns.import_btn.relative_rect.width
+    ns.export_btn = pgui.elements.UIButton(
+        relative_rect=pg.Rect((x, 0), (100, 50)),
+        text='Export',
+        manager=manager)
+
+    return ns
