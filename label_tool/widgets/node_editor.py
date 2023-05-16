89f373100120579764e2c5e2016f494f0ebfe7f3
Hùng Nguyễn <ndgnuh@protonmail.com>
2023-05-16 10:42:54 +0700

    what does your failure bring you? back to me

--- label_tool/widgets/node_editor.py
+++ label_tool/widgets/node_editor.py
@@ -22,51 +22,27 @@ class Node:
     x: int
     y: int
     node_id: ed.NodeId = field(default_factory=ed.NodeId.create)
-    t_pin: ed.PinId = field(default_factory=ed.PinId.create)
-    b_pin: ed.PinId = field(default_factory=ed.PinId.create)
-    l_pin: ed.PinId = field(default_factory=ed.PinId.create)
-    r_pin: ed.PinId = field(default_factory=ed.PinId.create)
-    tr_pin: ed.PinId = field(default_factory=ed.PinId.create)
-    tl_pin: ed.PinId = field(default_factory=ed.PinId.create)
-    br_pin: ed.PinId = field(default_factory=ed.PinId.create)
-    bl_pin: ed.PinId = field(default_factory=ed.PinId.create)
+    in_pin: ed.PinId = field(default_factory=ed.PinId.create)
+    out_pin: ed.PinId = field(default_factory=ed.PinId.create)
 
     def __hash__(self):
         return hash((self.text, self.x, self.y))
 
-    def draw_group(self, pins, texts):
-        imgui.begin_group()
-        longest = max(map(len, texts))
-        for (pin, text) in zip(pins, texts):
-            text = text * (longest // len(text))
-            if pin is not None:
-                ed.begin_pin(pin, ed.PinKind.output)
-                imgui.text(text)
-                ed.end_pin()
-            else:
-                imgui.text(text)
-        imgui.end_group()
-
     def draw(self):
-        pin_text = "*"
+        pin_text = "."
         ed.begin_node(self.node_id)
 
-        self.draw_group(
-            [self.tl_pin, self.l_pin, self.bl_pin],
-            [pin_text] * 3
-        )
+        ed.begin_pin(self.in_pin, ed.PinKind.input)
+        imgui.text(pin_text)
+        ed.end_pin()
 
         imgui.same_line()
-        self.draw_group(
-            [self.t_pin, None, self.b_pin],
-            [pin_text, self.text, pin_text]
-        )
+        imgui.text(self.text)
 
         imgui.same_line()
-        self.draw_group(
-            [self.tr_pin, self.r_pin, self.br_pin],
-            [pin_text] * 3
-        )
+        ed.begin_pin(self.out_pin, ed.PinKind.output)
+        imgui.text(pin_text)
+        ed.end_pin()
 
         ed.end_node()
 
@@ -78,43 +54,8 @@ class Link:
     to_node: Node
 
     def draw(self):
-        w1, h1 = ed.get_node_size(self.from_node.node_id)
-        w2, h2 = ed.get_node_size(self.to_node.node_id)
-        x1, y1 = ed.get_node_position(self.from_node.node_id)
-        x2, y2 = ed.get_node_position(self.to_node.node_id)
-        x1 = x1 + w1//2
-        x2 = x2 + w2//2
-        y1 = y1 + h1//2
-        y2 = y2 + h2//2
-        angle = math.atan2(y2 - y1, x2 - x1)
-        angle = (angle + math.pi) / math.pi / 2
-        angle = int(angle * 8 * 8 - 0.5) // 8
-        mapping = [
-            ("tl", "br"),
-            ("t", "b"),
-            ("tr", "bl"),
-            ("r", "l"),
-            ("br", "tl"),
-            ("b", "t"),
-            ("bl", "tr"),
-            ("l", "r"),
-        ]
-        try:
-            s, e = mapping[angle]
-        except Exception:
-            print("__")
-            print(y1, y2, x1, x2)
-            angle = math.atan2(y1 - y2, x1 - x2)
-            print('a1', angle)
-            angle = (angle + math.pi) / math.pi / 2
-            print('a2', angle)
-            angle = int(angle * 8 * 8) // 8
-            print('a3', angle)
-            return
-        s = f"{s}_pin"
-        e = f"{e}_pin"
-        from_pin = getattr(self.from_node, s)
-        to_pin = getattr(self.to_node, e)
+        from_pin = self.from_node.out_pin
+        to_pin = self.to_node.in_pin
         ed.link(ed.LinkId(self.link_id), from_pin, to_pin)
 
 
